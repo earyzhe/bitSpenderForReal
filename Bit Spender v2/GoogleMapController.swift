9af1f3a4d8e9986817983bd51b2e59f11db6e627
“earyzhe” <earyzhe@gmail.com>
2017-10-29 17:55:49 +0100

    initialCommit

---
+++ Bit Spender v2/GoogleMapController.swift
@@ -0,0 +1,40 @@
+//
+//  GoogleMapController.swift
+//  Bit Spender v2
+//
+//  Created by Andrew Johnson on 29.10.17.
+//  Copyright © 2017 Rinson. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class GoogleMapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        self.view = mapView
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bit Spender v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bit Spender v2
+//
+//  Created by Andrew Johnson on 29.10.17.
+//  Copyright © 2017 Rinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
